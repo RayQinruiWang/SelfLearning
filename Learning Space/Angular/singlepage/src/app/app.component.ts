9f2da73609a3549bd71ce746ee1f261dce6f335e
RayQinruiWang <42256106+RayQinruiWang@users.noreply.github.com>
2018-08-27 16:44:20 +1000

    Another attempt initiated

---
+++ Learning Space/Angular/singlepage/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items = ["Learn Angular", "Something", "something Else", "Make it talk to a DB", "Add multiple user"];
+  newItem = "";
+  pushItem = function(){
+    if(this.newItem != ""){
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem = function(index){
+    this.items.splice(index,1);
+  }
+}
