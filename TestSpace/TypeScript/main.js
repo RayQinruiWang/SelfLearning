6cbd4e5f9e5c02740bfdaae7d7b61aaf88fa3752
RayQinruiWang <42256106+RayQinruiWang@users.noreply.github.com>
2018-08-30 13:00:52 +1000

    TypeScript reinforced, back to angular

--- TestSpace/TypeScript/main.js
+++ TestSpace/TypeScript/main.js
@@ -1,5 +1,14 @@
+// keyword "let" can also be used just like in JS, but be careful about the scope difference
+var message = "Hello World from TypeScript";
+log(message);
+loop();
 function log(message) {
     console.log(message);
 }
-var message = "Hello World from TypeScript";
-log(message);
+;
+function loop() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finall i reached: " + i);
+}
